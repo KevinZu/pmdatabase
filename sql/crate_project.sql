1c9cc24e7bbc24bb0759e4330421ec96ecc78b82
root <root@localhost.localdomain>
2018-09-04 11:20:02 +0800

    add project table

--- sql/crate_project.sql
+++ sql/crate_project.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `task_example` (
+CREATE TABLE `project_table` (
     `id`            BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `create_time`          DOUBLE NOT NULL,
     `start_time`          DOUBLE NOT NULL,
     `finish_time`          DOUBLE NOT NULL,
-    `task_leader_id`       INT UNSIGNED NOT NULL,
+    `project_leader_id`       INT UNSIGNED NOT NULL,
     `f_id`            BIGINT UNSIGNED,
-    `task_name`         VARCHAR(30) NOT NULL,
+    `project_name`         VARCHAR(30) NOT NULL,
     `detail`        TEXT NOT NULL,
-    INDEX `idx_leader_task` (`task_leader_id`, `task_name`)
+    `status`    INT UNSIGNED
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
  
